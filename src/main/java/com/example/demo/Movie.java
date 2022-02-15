6400aa464276be33bcda166e0b5a32033163c7e6
Oriol Rafols <40451847+BeautifulPhoenix@users.noreply.github.com>
2022-02-15 20:22:44 +0100

    master

--- src/main/java/com/example/demo/Movie.java
+++ src/main/java/com/example/demo/Movie.java
@@ -1,5 +1,40 @@
 package com.example.demo;
 
+import java.time.Year;
+
 public class Movie {
 
+    private String title;
+    private int year;
+
+    public Movie() {
+    }
+
+    public Movie(String title, int year) {
+        super();
+        this.title = title;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie [title=" + title + ", year=" + year + "]";
+    }
 }
