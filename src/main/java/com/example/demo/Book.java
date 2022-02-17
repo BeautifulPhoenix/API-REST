5c665d3f885950f683bef78878c8834acd2dcd83
Oriol Rafols <40451847+BeautifulPhoenix@users.noreply.github.com>
2022-02-17 19:51:25 +0100

    master

--- src/main/java/com/example/demo/Book.java
+++ src/main/java/com/example/demo/Book.java
@@ -1,58 +1,103 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+
 public class Book {
-	
+
 	private String title;
 	private String author;
 	private int pages;
 	private int year;
 	private String ISBN;
-	
-	public Book () {}
-	
+	private LocalDate createdat;
+	private Date datePublished;
+
+	public Book() {
+	}
+
 	public Book(String title, String author, int pages, int year, String iSBN) {
 		super();
 		this.title = title;
 		this.author = author;
 		this.pages = pages;
 		this.year = year;
-		ISBN = iSBN;
+		this.ISBN = ISBN;
+		this.datePublished = datePublished;
 	}
-	
+
+	public Book(String title, String author, int pages, int year, String ISBN, Date datePublished,
+			LocalDate createdat) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.year = year;
+		this.ISBN = ISBN;
+		this.setCreatedat(createdat);
+		this.datePublished = datePublished;
+
+	}
+
+	public Date getDatePublished() {
+		return datePublished;
+	}
+
+	public void setDatePublished(Date datePublished) {
+		this.datePublished = datePublished;
+	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getAuthor() {
 		return author;
 	}
+
 	public void setAuthor(String author) {
 		this.author = author;
 	}
+
 	public int getPages() {
 		return pages;
 	}
+
 	public void setPages(int pages) {
 		this.pages = pages;
 	}
+
 	public int getYear() {
 		return year;
 	}
+
 	public void setYear(int year) {
 		this.year = year;
 	}
+
 	public String getISBN() {
 		return ISBN;
 	}
-	public void setISBN(String iSBN) {
-		ISBN = iSBN;
+
+	public void setISBN(String ISBN) {
+		this.ISBN = ISBN;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Book [title=" + title + ", author=" + author + ", pages=" + pages + ", year=" + year + ", ISBN=" + ISBN
 				+ "]";
 	}
+
+	public LocalDate getCreatedat() {
+		return createdat;
+	}
+
+	public void setCreatedat(LocalDate createdat) {
+		this.createdat = createdat;
+	}
+
 }
