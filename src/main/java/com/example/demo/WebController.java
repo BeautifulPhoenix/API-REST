01b40dfe5c11d6299bd121a905d9fa8cce0435ce
Oriol Rafols <40451847+BeautifulPhoenix@users.noreply.github.com>
2022-02-22 15:49:02 +0100

    master

--- src/main/java/com/example/demo/WebController.java
+++ src/main/java/com/example/demo/WebController.java
@@ -3,8 +3,6 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import ch.qos.logback.classic.Logger;
-
 @Controller
 @RequestMapping("/myLibrary")
 public class WebController {
