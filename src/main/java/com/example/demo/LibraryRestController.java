bc2e22eaadd42543bbcc23464a55ad7bce17fc2d
Oriol Rafols <40451847+BeautifulPhoenix@users.noreply.github.com>
2022-03-02 15:13:04 +0100

    Authors post

--- src/main/java/com/example/demo/LibraryRestController.java
+++ src/main/java/com/example/demo/LibraryRestController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("apilibrary")
 public class LibraryRestController {
-
 	@Autowired
 	BookService bookservice;
 	@Autowired
